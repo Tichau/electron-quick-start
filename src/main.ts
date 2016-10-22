60577969746a4571fa641be1ddeabc5779a03d28
Tichau <tsi.allard@gmail.com>
2016-10-22 23:20:56 +0200

    Add watch task for debug

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { app, BrowserWindow } from 'electron';
 import { Application } from './Application';
 
+// Reload the electron app on file changes!
+require('electron-reload')(__dirname);
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let application = new Application();
